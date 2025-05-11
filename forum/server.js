039829966834929ffce81b235059e6161f5c0f0c
DESKTOP-5NUN1M7\bhjeon <bhjeon@imbu.co.kr>
2025-05-12 08:32:28 +0900

    [코딩애플] Node.js, MongoDB 스터디 Part 1 (신버전) 1강 - 남자라면 서버개발을 할 줄 알아야함

---
+++ forum/server.js
@@ -0,0 +1,16 @@
+// TODO : 터미널 창 에서 명령어 "node server.js" 입력 및 엔터시 아래와 같은 오류 메시지 출력 시
+//        오류 메시지 "node : 'node' 용어가 cmdlet, 함수, 스크립트 파일 또는 실행할 수 있는 프로그램 이름으로 인식되지 않습니다. 
+//        이름이 정확한지 확인하고 경로가 포함된 경우 경로가 올바른지 검증한 다음 다시 시도하십시오."
+//        기본 터미널 창이 powershell이기 때문에 발생하는 오류 이므로, 
+//        1. ctrl+shift+p 누른 후 맨 위에 뜨는 Terminal:Select Default Profile 선택 or 검색창에 'Terminal:Select Default Profile' 치기
+//        2. Command Prompt로 설정 후 터미널 닫았다 다시 열기(Ctrl+`는 터미널 열기)
+//        3. 기본 터미널 변한거 확인하기
+// 참고 URL - https://yeon960.tistory.com/269
+
+// TODO : 터미널 창 에서 명령어 "node server.js" 입력 및 엔터시 오류 메시지 "'node'은(는) 내부 또는 외부 명령, 실행할 수 있는 프로그램, 또는 배치 파일이 아닙니다."
+//        출력시 시스템 환경 변수 편집 화면 들어가서 항목 Path에 node.js 경로 "C:\Program Files\nodejs" 추가하기 (2025.05.12 minjae)
+// 참고 URL - https://bbeomgeun.tistory.com/37
+
+// [코딩애플] Node.js, MongoDB 스터디 
+// Part 1 : (신버전)
+// 1강 - 남자라면 서버개발을 할 줄 알아야함
